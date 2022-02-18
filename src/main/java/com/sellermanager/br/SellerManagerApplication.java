1f5f766cb21cfe3d8f95ac13dc32db024a50f5aa
stephaniesantosss <stephanieps2016@hotmail.com>
2022-02-18 20:00:01 -0300

    initial setup

---
+++ src/main/java/com/sellermanager/br/SellerManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sellermanager.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerManagerApplication.class, args);
+	}
+
+}
